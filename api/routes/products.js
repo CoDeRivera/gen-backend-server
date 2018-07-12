ea6ec79b2e3627230bcad89858d514661925e0ea
CoDeRivera <CoDeRivera@users.noreply.github.com>
2018-07-11 21:17:03 -0400

    Update products.js

--- api/routes/products.js
+++ api/routes/products.js
@@ -10,9 +10,17 @@ router.get('/', (req,res) => {
 });
 //Handles the POST request from server.js /products path
 router.post('/', (req,res) => {
-    res.json({
-        message: 'Handling POST requests from /products'
+    //Create new product
+    const product = {
+        name: req.body.name,
+        price: req.body.price,
+    };
+    res.status(201).json({
+        message: 'Handling POST requests from /products',
+        //Display created product
+        createdProduct: product
     });
+    
 });
 
 //Handles the GET request from server.js /products path with
@@ -26,9 +34,27 @@ router.get('/:productId', (req,res) => {
         });
     }else{
             res.status(201).json({
-                message: 'You passed an ID'
+                message: 'You passed an ID',
             });
     }
+});
+
+//Handles the PATCH request from server.js /products path with
+//dynamic variable of productId
+router.patch('/:productId', (req,res) => {
+    const id = req.params.productId;
+    res.status(201).json({
+        message: 'Updated Product ' + id
+    });
+});
+
+//Handles the DELETE request from server.js /products path with
+//dynamic variable of productId
+router.delete('/:productId', (req,res) => {
+    const id = req.params.productId;
+    res.status(202).json({
+        message: id + ' was deleted'
+    });
 });
 
 module.exports = router;
